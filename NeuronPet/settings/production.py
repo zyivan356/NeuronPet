fbb173b9e19c51160aadba3acd316bc398fec472
Ivan <66207527+zyivan356@users.noreply.github.com>
2022-02-09 00:45:30 +0300

    Update production.py

--- NeuronPet/settings/production.py
+++ NeuronPet/settings/production.py
@@ -33,7 +33,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['0.0.0.0', 'neuronpet.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'neuronpet.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
