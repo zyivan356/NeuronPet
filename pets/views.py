f5c1052f76434669facbe1c45f419c11217a511d
Ivan <66207527+zyivan356@users.noreply.github.com>
2022-02-09 20:59:06 +0300

    Update views.py

--- pets/views.py
+++ pets/views.py
@@ -39,7 +39,7 @@ def neuron_network(request):
             form = PostForm()
 
 
-            data_dir = pathlib.Path("C:\\Users\\zyiva\\PycharmProjects\\NeuronPet\\pets\\dataset\\training_set")
+            data_dir = pathlib.Path("s3://data-set-storage/dataset/training_set/")
 
             image_count = len(list(data_dir.glob('*/*.jpg')))
 
