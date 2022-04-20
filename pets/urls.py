4720023fab3ed9bfbb617a493a609199a8ea2596
Ivan <66207527+zyivan356@users.noreply.github.com>
2022-04-21 01:13:09 +0300

    Update urls.py

--- pets/urls.py
+++ pets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from pets.views import main, pets_owner_list, pet_detail, neuron_network
+from pets.views import main, pets_owner_list, pet_detail, neuron_network, pet_create
 
 urlpatterns = [
     path('', main, name='main'),
     path('mypets/', pets_owner_list, name='pets_owner_list'),
     path('pet/<int:id>/', pet_detail, name='pet_detail'),
     path('pet_neuron/', neuron_network, name="neuron_network"),
+    path('pet_add/', pet_create, name="pet_create"),
 ]
 
